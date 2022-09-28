a4ef078622c385fb4222826557f061ed17b92f7e
Matt Duggan <matthew.duggan@skybettingandgaming.com>
2022-09-28 11:23:03 +0100

    Changed database from test data to development data

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -5,10 +5,13 @@ const Nav = () => {
         <nav className="navBar">
             <Link to="/">Home</Link><br />
             <Link to="/reviews">All Reviews</Link> <br />
-            <Link to="/reviews/category/euro_game"> Euro Games</Link> <br />
-            <Link to="/reviews/category/social_deduction">Social Deduction Games</Link><br />
+            <Link to="/reviews/category/strategy"> Strategy Games</Link> <br />
+            <Link to="/reviews/category/hidden-roles">Hidden Roles Games</Link><br />
             <Link to="/reviews/category/dexterity">Dexterity Games</Link><br />
-            <Link to="/reviews/category/childrens">Children's Games</Link><br />
+            <Link to="/reviews/category/push-your-luck">Push Your Luck Games</Link><br />
+            <Link to="/reviews/category/roll-and-write">Roll And Write Games</Link><br />
+            <Link to="/reviews/category/deck-building">Deck Building Games</Link><br />
+            <Link to="/reviews/category/engine-building">Engine Building Games</Link><br />
             <Link to="/">Users</Link> <br />
         </nav>
     )
